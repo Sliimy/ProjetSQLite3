78a3e74f6317ced6ee0a33901aeb17f9cbf0ea60
sliimy <real-seb@hotmail.fr>
2017-10-12 15:42:05 +0000

    Deplacement fichier dans repertoir cmake

--- gestionDB.h
+++
@@ -1,3 +0,0 @@
-void createNewTable(char nameTable[],int nbrInput,sqlite3 *db);
-static int callback(void *NotUsed, int argc, char **argv, char **azColName);
-void setDataDB(char nameTable[],int data[],int sizeTabl,sqlite3 *sql);
