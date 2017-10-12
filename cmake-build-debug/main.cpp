090a310607de865ffceabf69166092e7c2eee4e7
sliimy <real-seb@hotmail.fr>
2017-10-12 18:14:05 +0000

    Creation fichier crateTable.cpp/.h + Fonction phase1

--- cmake-build-debug/main.cpp
+++ cmake-build-debug/main.cpp
@@ -1,12 +1,10 @@
-#include <stdio.h>
+
 #include <sqlite3.h>
 #include <string>
-#include <string.h>
 #include <iostream>
-#include "gestionDB.h"
-#include "convert.h"
+#include "createTable.h"
 
-#define NBR_INPUT 3
+
 
 // AJOUT FOREIGN KEY POUR LES SORTIES
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
@@ -27,30 +25,7 @@ int main() {
 
     printf("Je suis dedans\n");
     rc = sqlite3_open("mainP.db", &db);
-
-    char nameTable[]="table1";
-    createNewTable( nameTable,NBR_INPUT,db);
-    int nbrLineDB=1;
-    nbrLineDB=nbrLineDB<<NBR_INPUT;
-    printf("nbrLine : %d\n",nbrLineDB);
-    int i;
-    int tabBinary[NBR_INPUT];
-    int data[NBR_INPUT+1];
-    for (i=0;i<nbrLineDB;i++){
-        intToTableBinary(i,tabBinary,NBR_INPUT);
-        data[0]=i;
-        printf("tabBinary :%d  %d  %d \n",tabBinary[0],tabBinary[1],tabBinary[2]);
-        printf("Predata :%d  %d  %d %d \n",data[0],data[1],data[2],data[3]);
-
-        int j;
-        memcpy(&data[1],tabBinary, sizeof(tabBinary));
-        printf("data :%d  %d  %d %d \n\n",data[0],data[1],data[2],data[3]);
-
-        setDataDB(nameTable,data,NBR_INPUT+1,db);
-
-    }
-
-
+    phase1(db);
     sqlite3_close(db);
 
     return 0;
