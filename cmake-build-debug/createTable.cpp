8b5cb52281f82d5c5c08d6319353cf1ed2232ba7
sliimy <real-seb@hotmail.fr>
2017-10-14 16:59:45 +0000

    Creation fichier gitignore

--- cmake-build-debug/createTable.cpp
+++ cmake-build-debug/createTable.cpp
@@ -20,29 +20,29 @@ void phase1(sqlite3 *db) {
         setDataDB(nameTable,tabBinary, NBR_INPUT, db);
     }
 }
+//TODO: Reprendre cette fonction 
+void phase2(sqlite3 *db) {
 
-//void phase2(sqlite3 *db) {
-//
-//    char nameTable[]="table2.x";
-//    int nbrLineDB = 1;
-//    nbrLineDB = nbrLineDB << NBR_INPUT;
-//    int i;
-//    for(i=0;i<nbrLineDB;i++){
-//        sprintf(&nameTable[sizeof(nameTable)-1],"%d",i);
-//        createNewTable( nameTable,NBR_INPUT,db);
-//        int j;
-//        int tabBinary[NBR_INPUT];
-//        int data1[NBR_INPUT + 1];
-//        int data2[NBR_INPUT + 1];
-//        for (j = 0; j < nbrLineDB; j++) {
-//            intToTableBinary(j, tabBinary, NBR_INPUT);
-//            setDataDB(nameTable, tabBinary, NBR_INPUT, db);
-//            int k;
-//            for(k=0; k<nbrLineDB;k++){
-//                intToTableBinary(j, tabBinary, NBR_INPUT);
-//                data2[0] = j;
-//                setDataDB(nameTable,tabBinary, NBR_INPUT, db);
-//            }
-//        }
-//    }hbkj
-//}
+    char nameTable[]="table2.x";
+    int nbrLineDB = 1;
+    nbrLineDB = nbrLineDB << NBR_INPUT;
+    int i;
+    for(i=0;i<nbrLineDB;i++){
+        sprintf(&nameTable[sizeof(nameTable)-1],"%d",i);
+        createNewTable( nameTable,NBR_INPUT,db);
+        int j;
+        int tabBinary[NBR_INPUT];
+        int data1[NBR_INPUT + 1];
+        int data2[NBR_INPUT + 1];
+        for (j = 0; j < nbrLineDB; j++) {
+            intToTableBinary(j, tabBinary, NBR_INPUT);
+            setDataDB(nameTable, tabBinary, NBR_INPUT, db);
+            int k;
+            for(k=0; k<nbrLineDB;k++){
+                intToTableBinary(j, tabBinary, NBR_INPUT);
+                data2[0] = j;
+                setDataDB(nameTable,tabBinary, NBR_INPUT, db);
+            }
+        }
+    }
+}
